27a3fe5290d63a551dbd8d2ff2b1dc70525574ff
timealiver <timealive@mail.ru>
2024-07-08 00:37:42 +0300

    feat: add auth controller and router

---
+++ auth-service/src/controllers/authController.js
@@ -0,0 +1,25 @@
+class authController {
+    async registration(req,res){
+        try {
+            
+        } catch (e) {
+            
+        }
+    }
+    async login(req,res){
+        try {
+            
+        } catch (e) {
+            
+        }
+    }
+    async getUsers(req,res){
+        try {
+            res.json("res work")
+        } catch (e) {
+            
+        }
+    }
+}
+
+module.exports = new authController();
---
+++ auth-service/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const authRouter=require('./routes/authRouter.js')
+const PORT = process.env.PORT || 5000;
+
+const app=express();
+app.use("/auth",authRouter);
+app.use(express.json())
+const start = async () => {
+try {
+    await mongoose.connect(`mongodb+srv://timealive:qwerty123@habits-1.kykynam.mongodb.net/?retryWrites=true&w=majority&appName=Habits-1`);
+    app.listen(PORT,()=> console.log(`Server started on port ${PORT}`))
+} catch (e) {
+    console.log(e);
+}
+}
+start();
