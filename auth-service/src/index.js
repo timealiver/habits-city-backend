20e2115388880c21e9ca717d74b532de359c3953
timealiver <timealive@mail.ru>
2024-07-09 00:09:19 +0300

    feat: add registration function to auth controller

--- auth-service/src/index.js
+++ auth-service/src/index.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 5000;
 
 const app=express();
 app.use("/auth",authRouter);
-app.use(express.json())
 const start = async () => {
 try {
     await mongoose.connect(`mongodb+srv://timealive:qwerty123@habits-1.kykynam.mongodb.net/auth_roles?retryWrites=true&w=majority&appName=Habits-1`);
