797e58fcd51c1473bb44b1aad8cea3316fc0e537
molsrg <molsrg.35@gmail.com>
2024-07-09 21:12:42 +0300

    feat: add CORS policy

--- auth-service/src/index.js
+++ auth-service/src/index.js
@@ -1,18 +1,30 @@
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose')
-const authRouter=require('./routes/authRouter.js')
-const PORT = process.env.PORT || 5000;
+const authRouter = require('./routes/authRouter.js')
+const cors = require('cors') // добавить middleware cors
 
-const app=express();
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(
+	cors({
+		origin: 'http://localhost:3000', // разрешить запросы с домена http://localhost:3000
+	})
+)
 
 app.use(express.json())
-app.use("/auth",authRouter);
+app.use('/auth', authRouter)
+
 const start = async () => {
-try {
-    await mongoose.connect(`mongodb+srv://timealive:qwerty123@habits-1.kykynam.mongodb.net/auth_roles?retryWrites=true&w=majority&appName=Habits-1`);
-    app.listen(PORT,()=> console.log(`Server started on port ${PORT}`))
-} catch (e) {
-    console.log(e);
+	try {
+		await mongoose.connect(
+			`mongodb+srv://timealive:qwerty123@habits-1.kykynam.mongodb.net/auth_roles?retryWrites=true&w=majority&appName=Habits-1`
+		)
+		app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+	} catch (e) {
+		console.log(e)
+	}
 }
-}
-start();
+
+start()
