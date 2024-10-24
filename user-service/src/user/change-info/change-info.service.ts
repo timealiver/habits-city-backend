b8b26af6d3552aab929b3038fcbca957afb9385a
timealiver <timealive@mail.ru>
2024-10-24 14:07:40 +0300

    feat: add changeAvatar route via s3 connect

--- user-service/src/user/change-info/change-info.service.ts
+++ user-service/src/user/change-info/change-info.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post, UseInterceptors } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/models/user.model';
+import * as AWS from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class ChangeInfoService {}
+export class ChangeInfoService {
+    private s3: AWS.S3;
+    constructor (@InjectModel(User.name) private userModel: Model<User>){
+        AWS.config.update({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            region: 'ru-central1', 
+          });
+      
+          this.s3 = new AWS.S3({
+            endpoint: 'hb.ru-msk.vkcloud-storage.ru', 
+          });
+    }
+    async changeAvatar(file: Express.Multer.File,userId:string,){
+
+      try {
+        const user = await this.userModel.findById(userId);
+        if (!user){
+          console.log(userId);
+          throw Error('User not found');
+        }
+        const key = `avatars/${uuidv4()}`;
+  
+        await this.s3.putObject({
+          Body: file.buffer,
+          Bucket: 'users-photo',
+          Key: key,
+          ContentType: file.mimetype,
+          ACL: 'public-read'
+        }).promise();
+  
+        const fileUrl = `https://hb.ru-msk.vkcloud-storage.ru/users-photo/${key}`;
+  
+        // Обновите информацию о пользователе в базе данных
+        if (user.avatar!=null){
+          const key = user.avatar.split('/')[5];
+          await this.s3.deleteObject({
+            Bucket: 'users-photo',
+            Key: 'avatars/'+key,
+          }).promise();
+        }
+        user.avatar = fileUrl;
+        await user.save();
+  
+        return { url: fileUrl };
+      } catch (error) {
+        console.error('Error uploading file:', error);
+        throw new Error('Error uploading file');
+      }
+    }
+}
--- user-service/src/user/user.controller.ts
+++ user-service/src/user/user.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, UseGuards, Request, Param, Query } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Param, Query, UseInterceptors, Post, UploadedFile, Body } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { UserInfoService } from './user-info/user-info.service';
 import { UserInfoDto } from 'src/dto/user-info.dto';
 import { SearchInfoDto } from 'src/dto/search-info.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ChangeInfoService } from './change-info/change-info.service';
 @Controller('user')
 export class UserController {
-    constructor(private readonly userInfoService: UserInfoService) {}
+    constructor(
+        private readonly userInfoService: UserInfoService, 
+        private readonly changeInfoService: ChangeInfoService
+    ) {}
 
 @Get("getInfo")
 @UseGuards(AuthGuard)
@@ -23,9 +28,16 @@ async getSearchUsers(@Query('username') username: string):Promise<SearchInfoDto[
 @Get("isTaken")
 @UseGuards(AuthGuard)
 async isUsernameTaken(@Query('username') username: string): Promise<boolean>{
-    console.log("U: ",username);
     return this.userInfoService.isUsernameTaken(username);
 }
+
+@Post("changeAvatar")
+@UseGuards(AuthGuard)
+@UseInterceptors(FileInterceptor('avatar'))
+async changeAvatar(@UploadedFile() file: Express.Multer.File, @Request() request) {
+    const userId = request.user.userId;
+    return this.changeInfoService.changeAvatar(file, userId);
+  }
 
 
 }
