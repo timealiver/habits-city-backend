29dceb84457d5e1ea8ba3e466bb4b39e7f201c7e
timealiver <timealive@mail.ru>
2024-11-21 18:57:16 +0300

    fix: small changes in locale file

--- auth-service/src/interfaces/response.js
+++ auth-service/src/interfaces/response.js
@@ -8,11 +8,17 @@ class ApiResponse {
   }
 
   static createSuccess(locale, code, data) {
+    if (!locale) {
+      locale = en;
+    }
     const message = MESSAGES[locale]?.success[code];
     return new ApiResponse('success', code, message, data);
   }
 
   static createError(locale, code, data) {
+    if (!locale) {
+      locale = 'en';
+    }
     const message = MESSAGES[locale]?.error[code];
     return new ApiResponse('error', code, message, data);
   }
--- auth-service/src/utils/localeMessages.js
+++ auth-service/src/utils/localeMessages.js
@@ -7,8 +7,12 @@ const MESSAGES = {
     error: {
       SAME_USERNAME: 'Username is already in use',
       USERNAME_EMPTY: "Username can't be empty",
+      USERNAME_SHORT: 'Username should contain at least 4 symbols',
+      USERNAME_INVALID:
+        'Username should contain only letters, numbers or underscore',
       PASSWORD_SHORT: "Password can't be shorter than 6 symbols",
       PASSWORD_NUM: 'Password should contain at least one number',
+      PASSWORD_CAPITAL: 'Password should contain at least one capital letter',
     },
   },
   ru: {
@@ -19,8 +23,13 @@ const MESSAGES = {
     error: {
       SAME_USERNAME: 'Пользователь с таким именем уже существует',
       USERNAME_EMPTY: 'Имя пользователя не может быть пустым',
+      USERNAME_SHORT:
+        'Имя пользователя должнодолжно содержать минимум 4 символа',
+      USERNAME_INVALID:
+        'Имя пользователя должно содержать только латиницу, цифры или _',
       PASSWORD_SHORT: 'Пароль не может быть короче 6 символов',
       PASSWORD_NUM: 'Пароль должен содержать хотя бы одну цифру',
+      PASSWORD_CAPITAL: 'Пароль должен содержать хотя бы одну заглавную букву',
     },
   },
 };
